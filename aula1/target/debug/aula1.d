1dd090d10c5ad208c218d1111f7dcf67a8a88962
ronanfelicio <ronanoliver002@gmail.com>
2025-02-14 22:09:39 +0000

    feat: commit inicial

---
+++ aula1/target/debug/aula1.d
@@ -0,0 +1,1 @@
+/workspaces/ronan.oliveira3na/aula1/target/debug/aula1: /workspaces/ronan.oliveira3na/aula1/src/main.rs
---
+++ aula1/target/debug/deps/aula1-0feb40cd98cdc893.d
@@ -0,0 +1,5 @@
+/workspaces/ronan.oliveira3na/aula1/target/debug/deps/aula1-0feb40cd98cdc893: src/main.rs
+
+/workspaces/ronan.oliveira3na/aula1/target/debug/deps/aula1-0feb40cd98cdc893.d: src/main.rs
+
+src/main.rs:
